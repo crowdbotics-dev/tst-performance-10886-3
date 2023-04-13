29d9e683af0334c7ed2dc9c863c96158bf8f1e43
crowdbotics-dev <engineering@crowdbotics.com>
2023-04-13 11:44:36 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import SubMenu
+admin.site.register(SubMenu)
 
 # Register your models here.
--- backend/main_menu/views.py
+++ backend/main_menu/views.py
@@ -1,3 +1,5 @@
+from .models import Menu
+from django.views.generic import CreateView, UpdateView, DetailView, ListView
 from django.shortcuts import render
 
 # Create your views here.
